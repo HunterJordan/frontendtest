fedf9f9a7874e58252af76efca68cf3585db3b17
HunterJordan <hunterg96@gmail.com>
2021-08-27 02:04:42 -0500

    asdasd

--- song_suggester/__init__.py
+++ song_suggester/__init__.py
@@ -1,10 +1,10 @@
 '''entry-point for app'''
 
 from .app import create_app
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 
-load_dotenv('.flaskenv')
-load_dotenv('.env')
+#load_dotenv('.flaskenv')
+#load_dotenv('.env')
 
 APP = create_app()
